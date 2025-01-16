600e01d5cacafccd9e61d6d362cb4947bd2bc9f2
OmarNaru <nadaelomr391@gmail.com>
2025-01-16 13:46:44 +0200

    implemented worker service to delete inactive refresh tokens

---
+++ AggarApi/ApiWorker/Worker.cs
@@ -0,0 +1,37 @@
+using Dapper;
+using Microsoft.Data.SqlClient;
+
+namespace ApiWorker
+{
+    public class Worker : BackgroundService
+    {
+        private readonly ILogger<Worker> _logger;
+        private readonly IConfiguration _configuration;
+
+        public Worker(ILogger<Worker> logger, IConfiguration configuration)
+        {
+            _logger = logger;
+            _configuration = configuration;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                var connectionString = _configuration.GetConnectionString("default");
+                if (connectionString != null)
+                    DeleteRevokedOrExpiredRefreshTokens(connectionString);
+                var delay = TimeSpan.FromDays(1);
+                await Task.Delay(delay, stoppingToken); ;//run script 1 time a day
+            }
+        }
+        private void DeleteRevokedOrExpiredRefreshTokens(string constr)
+        {
+            using var connection = new SqlConnection(constr);
+
+            var sql = "delete from RefreshToken where RevokedOn is not null or ExpiresOn<CURRENT_TIMESTAMP";
+
+            connection.Execute(sql);
+        }
+    }
+}
