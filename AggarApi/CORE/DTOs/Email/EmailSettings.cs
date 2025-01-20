e428d46060e158ab703edecdf66bb737e3a70baa
OmarNaru <nadaelomr391@gmail.com>
2025-01-20 20:57:41 +0200

    removed environmentVariablesHelper, solve a tiny bug in getting address using location

--- AggarApi/CORE/DTOs/Email/EmailSettings.cs
+++ AggarApi/CORE/DTOs/Email/EmailSettings.cs
@@ -11,5 +11,7 @@ namespace CORE.DTOs.Email
         public string SmtpHost { get; set; }
         public int SmtpPort { get; set; }
         public string DisplayName { get; set; }
+        public string EmailAddress { get; set; }
+        public string Password { get; set; }
     }
 }
