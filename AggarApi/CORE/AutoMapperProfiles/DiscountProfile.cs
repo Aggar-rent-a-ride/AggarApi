2ce8a997e56b647c9eb6b25f788301ce2dcba6e5
OmarNaru <nadaelomr391@gmail.com>
2025-02-02 18:53:54 +0200

    mapped discount with it's dto, edited some bugs related to discount in VehicleProfile

---
+++ AggarApi/CORE/AutoMapperProfiles/DiscountProfile.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using CORE.DTOs.Vehicle;
+using DATA.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CORE.AutoMapperProfiles
+{
+    public class DiscountProfile: Profile
+    {
+        public DiscountProfile()
+        {
+            CreateMap<Discount, DiscountDto>()
+                .ForMember(dest => dest.DiscountedPricePerDay, opt => opt.MapFrom(src => decimal.Round(src.Vehicle.PricePerDay - (src.Vehicle.PricePerDay * src.DiscountPercentage / 100), 2, MidpointRounding.AwayFromZero)))
+                .ReverseMap();
+        }
+    }
+}
