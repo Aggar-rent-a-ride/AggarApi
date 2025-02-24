05563ef6688647426aeb6798e1c1429cd9166417
OmarNaru <nadaelomr391@gmail.com>
2025-02-24 20:14:27 +0200

    renamed Seen column in messages table to IsSeen

---
+++ AggarApi/DATA/DataAccess/Migrations/20250224181154_RenamedSeenInMessageToIsSeen.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DATA.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class RenamedSeenInMessageToIsSeen : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Seen",
+                table: "Messages",
+                newName: "IsSeen");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "IsSeen",
+                table: "Messages",
+                newName: "Seen");
+        }
+    }
+}
