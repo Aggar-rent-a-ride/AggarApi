e57a61c4261fa9d12630af8a4efefe1679b58cbc
OmarNaru <nadaelomr391@gmail.com>
2025-03-10 02:51:15 +0200

    minor change in review model, changed from datetime.now to datetime.utcnow

--- AggarApi/DATA/Models/Review.cs
+++ AggarApi/DATA/Models/Review.cs
@@ -4,7 +4,7 @@
     {
         public int Id { get; set; }
         public int RentalId { get; set; }
-        public DateTime CreatedAt { get; set; } = DateTime.Now;
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
         public double Behavior { get; set; }
         public double Punctuality { get; set; }
         public string? Comments { get; set; }
