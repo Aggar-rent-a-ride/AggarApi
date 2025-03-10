35dc652d32cdc11b0e00ebd211402ba47546eaaf
Mohamed03Said <eldeeb33442003@gmail.com>
2025-03-10 23:35:24 +0200

    Remove Price from Customer Review

--- AggarApi/DATA/Models/CustomerReview.cs
+++ AggarApi/DATA/Models/CustomerReview.cs
@@ -3,7 +3,6 @@
     public class CustomerReview : Review
     {
         public double Truthfulness { get; set; }
-        public double Price { get; set; }
         public int CustomerId { get; set; }
 
         public Customer Customer { get; set; } = null!;
