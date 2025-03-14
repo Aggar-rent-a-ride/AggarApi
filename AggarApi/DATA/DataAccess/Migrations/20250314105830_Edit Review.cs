8cba8908c3709c21823adc9a7f56603c2b66c629
Mohamed03Said <eldeeb33442003@gmail.com>
2025-03-14 13:00:09 +0200

    Migration for Review Edits

---
+++ AggarApi/DATA/DataAccess/Migrations/20250314105830_Edit Review.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DATA.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class EditReview : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Price",
+                table: "CustomerReviews");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<double>(
+                name: "Price",
+                table: "CustomerReviews",
+                type: "float",
+                nullable: false,
+                defaultValue: 0.0);
+        }
+    }
+}
