609a19aaed9e58348ad31fa7a7558c39fc872b58
Mohamed03Said <mohamedsaid03.dev@gmail.com>
2025-04-26 13:27:25 +0300

    Seed Some Vehicle Types

---
+++ AggarApi/DATA/DataAccess/Migrations/20250426095903_Seed Some Vehicle Types.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DATA.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedSomeVehicleTypes : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("UPDATE VehicleTypes SET SlogenPath = 'Images/VehicleTypes/motorcycles.svg' WHERE Name = 'Motorcycle'");
+
+            migrationBuilder.InsertData(
+                table: "VehicleTypes",
+                columns: new[] { "Name", "SlogenPath" },
+                values: new object[,]
+                {
+                    { "Recreational", "Images/VehicleTypes/recreational.svg" },
+                    { "Taxi", "Images/VehicleTypes/taxi.svg" },
+                    { "Van", "Images/VehicleTypes/van.svg" },
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
